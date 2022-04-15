77cc96105301753c101bef71462ff3bfe54e9d3e
BongGyoeng Park <iampbk7@gmail.com>
2022-04-15 18:39:37 +0900

    python 3 migration

--- scripts/triad_openvr.py
+++ scripts/triad_openvr.py
@@ -155,10 +155,12 @@ class vr_tracked_device():
         self.vr = vr_obj
         
     def get_serial(self):
-        return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_SerialNumber_String).decode('utf-8')
+        # return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_SerialNumber_String).decode('utf-8')
+        return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_SerialNumber_String)
     
     def get_model(self):
-        return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_ModelNumber_String).decode('utf-8')
+        # return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_ModelNumber_String).decode('utf-8')
+        return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_ModelNumber_String)
         
     def sample(self,num_samples,sample_rate):
         interval = 1/sample_rate
@@ -195,7 +197,8 @@ class vr_tracked_device():
         return tracking
 class vr_tracking_reference(vr_tracked_device):
     def get_mode(self):
-        return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_ModeLabel_String).decode('utf-8').upper()
+        # return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_ModeLabel_String).decode('utf-8').upper()
+        return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_ModeLabel_String).upper()
     def sample(self,num_samples,sample_rate):
         print("Warning: Tracking References do not move, sample isn't much use...")
         
--- scripts/vive_tracker.py
+++ scripts/vive_tracker.py
@@ -32,7 +32,7 @@ def vive_tracker():
       else:
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
-        print message
+        print(message)
       #print(ex.args)
       quit()
 
