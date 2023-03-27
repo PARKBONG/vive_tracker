732708acd42d773e12019c8ac414431581b4a72c
BongGyeong Park <39259734+PARKBONG@users.noreply.github.com>
2023-03-27 22:43:08 +0900

    changed

--- scripts/bong_nohmd.py
+++ scripts/bong_nohmd.py
@@ -3,7 +3,7 @@ from std_msgs.msg import Float32MultiArray
 import triad_openvr
 from tf.transformations import quaternion_multiply
 from scipy.spatial.transform import Rotation as R
-
+import numpy as np
 from tf.transformations import euler_from_matrix
 
 v = triad_openvr.triad_openvr()
@@ -21,7 +21,7 @@ class Interp():
             self.old[i] = self.old[i] + interp_dtheta[0]
         return self.old
 
-def float_list_publisher(orientation_order):
+def float_list_publisher():
     # Initialize the ROS node
     rospy.init_node('float_list_publisher')
 
@@ -30,10 +30,16 @@ def float_list_publisher(orientation_order):
 
     # Set the publishing rate
     rate = rospy.Rate(90)
-    print("start to publish nZ euler with LARGER domain")
+    print("start to publish without headset")
     print("topic name : tracker_pose")
     print("node name : float_list_publisher")
 
+    Tracker_to_my = np.array([[-1,0,0],[0,0,-1],[0,-1,0]])
+    my_to_sim = np.array([[1,0,0],[0,-1,0],[0,0,-1]])
+    # tracker_to_my_trans = np.array([0,0.5, 1.5])
+    tracker_to_my_trans = np.array([0,-0.6, -1.3])
+    nYnZ = np.array([1,-1,-1])
+    multiplier = 100
     more_than_2pi = Interp()
     while not rospy.is_shutdown():
         ##### Create a Float32MultiArray message object
@@ -41,26 +47,16 @@ def float_list_publisher(orientation_order):
 
         ##### get device pose
         b = v.devices["tracker_1"].get_pose_quaternion()
-
-        ##### to the local pose
-        bong  = quaternion_multiply(b[3:], [-0.7071068, 0, 0, 0.7071068])
-    
         
-        ##### for euler publish
-        # o = R.from_quat(bong.tolist()).as_euler("YXZ", degrees = False).tolist()
-        o = R.from_quat(bong.tolist()).as_euler(orientation_order, degrees = False).tolist()
-
-        ##### ##### for nZ
-        b[2] = -b[2]
-        o[2] = -o[2]
-
+        # Real # -nY, -nZ
+        rot_my = (nYnZ * R.from_matrix(Tracker_to_my @ R.from_quat(b[3:]).as_matrix()).as_euler("XYZ", degrees=False)).tolist()
+        trans_my = (multiplier * nYnZ * ((Tracker_to_my @ np.array(b[:3]).T) + tracker_to_my_trans)).tolist()
+        
         ##### expand domain
-        o = more_than_2pi.euler(o)
+        rot_my = more_than_2pi.euler(rot_my)
+
         ##### publish
-        # msg.data = b[:3] + bong.tolist() # for quat publish
-        # print(o)
-        msg.data = b[:3] + o # for nZ euler publish
-        
+        msg.data = trans_my + rot_my 
 
         pub.publish(msg)
 
@@ -70,6 +66,25 @@ def float_list_publisher(orientation_order):
 if __name__ == '__main__':
     import sys
     try:
-        float_list_publisher(sys.argv[1])
+        float_list_publisher()
     except rospy.ROSInterruptException:
         pass
+
+# orientation
+#
+# b = v.devices["tracker_1"].get_pose_quaternion()
+# TOPVIEW
+#
+#       Z
+#       ^
+#       |
+#  X <--*
+#
+#
+# rot_my = R.from_matrix(Tracker_to_my @ R.from_quat(b[3:]).as_matrix()).as_euler("XYZ", degrees=False).tolist()
+# TOPVIEW       
+#
+#       *--> X
+#       |
+#       v
+#       Y
